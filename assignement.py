44e8b5cc689622b529827ab9520222d290801fb2
bhanu3397 <bhanu3397@gmail.com>
2020-05-18 22:02:45 -0500

    Added

--- assignement.py
+++ assignement.py
@@ -57,3 +57,43 @@ print(format_name("Voltaire", ""))
 
 print(format_name("", ""))
 # Should return an empty string
+#The multiplication_table function prints the results of a number passed to it multiplied by 1 through 5. 
+# An additional requirement is that the result is not to exceed 25, which is done with the break statement. 
+# Fill in the blanks to complete the function to satisfy these conditions.
+def multiplication_table(number):
+	# Initialize the starting point of the multiplication table
+	multiplier = 1
+	# Only want to loop through 5
+	while multiplier <= 5:
+		result = number*multiplier 
+		# What is the additional condition to exit out of the loop?
+		if result >25:
+			break
+		print(str(number) + "x" + str(multiplier) + "=" + str(result))
+		# Increment the variable for the loop
+		multiplier+= 1
+
+multiplication_table(3) 
+# Should print: 3x1=3 3x2=6 3x3=9 3x4=12 3x5=15
+
+multiplication_table(5) 
+# Should print: 5x1=5 5x2=10 5x3=15 5x4=20 5x5=25
+
+multiplication_table(8)	
+# Should print: 8x1=8 8x2=16 8x3=24
+
+
+def sum_divisors(n): 
+    sum = 0
+    z = 1
+
+    while n > z:
+        if n % z == 0:
+            sum = sum + z
+            z = z + 1
+        else:
+            z = z + 1
+    # Return the sum of all divisors of n, not including n
+    return sum
+print(sum_divisors(0)) 
+print(sum_divisors(45))
