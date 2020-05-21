c8523e60c3bcb5043df8aed43f9efaa6f366b69a
bhanu3397 <bhanu3397@gmail.com>
2020-05-21 00:42:08 -0500

    Added more

--- assignement.py
+++ assignement.py
@@ -97,3 +97,70 @@ def sum_divisors(n):
     return sum
 print(sum_divisors(0)) 
 print(sum_divisors(45))
+
+def square(n):
+    return n*n
+
+def sum_squares(x):
+    sum = 0
+    for n in range(1,x):
+        print(square(n))
+        sum += square(n)
+    return sum
+
+print(sum_squares(10)) # Should be 285
+
+
+def factorial(n):
+    result = 1
+    for i in range(1,n+1):
+        result = i*result
+    return result
+
+print(factorial(4)) # should return 24
+print(factorial(5)) # should return 120
+
+# Nested loops
+teams = ['dragons','pandas','wolves','unicorns']
+for home_team in teams:
+    for away_team in teams:
+        if home_team != away_team:
+            print(home_team+" VS "+away_team)
+
+#ans for Q1:
+def factorial(n):
+    result = 1
+    for x in range(1,n+1):
+        result = x*result
+    return result
+for n in range(0,10):
+    print(n, factorial(n))
+
+#ans for Q2:
+def retry(operation, attempts):
+  for n in range(attempts):
+    if operation():
+      print("Attempt " + str(n) + " succeeded")
+      break
+    else:
+      print("Attempt " + str(n) + " failed")
+
+retry(create_user, 3)
+retry(stop_service, 5)
+
+#ans for Q3
+for x in range(0,15):
+  print(x*7)
+
+
+#ans for Q4
+def retry(operation, attempts):
+  for n in range(attempts):
+    if operation():
+      print("Attempt " + str(n) + " succeeded")
+      break
+    else:
+      print("Attempt " + str(n) + " failed")
+
+retry(create_user, 3)
+retry(stop_service, 5)
