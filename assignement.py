201a5edeb5ae1c4d2b5aa2886884bc9bc47c4204
bhanu3397 <bhanu3397@gmail.com>
2020-06-06 18:38:31 -0500

    Added additional code

--- assignement.py
+++ assignement.py
@@ -164,3 +164,17 @@ def retry(operation, attempts):
 
 retry(create_user, 3)
 retry(stop_service, 5)
+
+##List comprehension: Let us create new list based on sequences(tuple,string or amy python sequence) or range ##
+
+#The odd_numbers function returns a list of odd numbers between 1 and n, inclusively. 
+# Fill in the blanks in the function, using list comprehension. 
+# Hint: remember that list and range counters start at 0 and end at the limit minus 1.
+def odd_numbers(n):
+	return [x for x in range(n+1) if x%2==1]
+
+print(odd_numbers(5))  # Should print [1, 3, 5]
+print(odd_numbers(10)) # Should print [1, 3, 5, 7, 9]
+print(odd_numbers(11)) # Should print [1, 3, 5, 7, 9, 11]
+print(odd_numbers(1))  # Should print [1]
+print(odd_numbers(-1)) # Should print []
