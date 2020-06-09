543d386254798a217faeef93692e4b0299013e6c
bhanu3397 <bhanu3397@gmail.com>
2020-06-09 00:30:12 -0500

    Added more

---
+++ test.py
@@ -0,0 +1,8 @@
+#We need to process a list of Event objects using their attributes to generate a report that lists all users currently logged in to the machines.
+#sort(), sorted()
+
+# 1 no punctuation marks
+# 2 keep it intresting
+#cloud = wordcloud.WordCloud()
+#cloud.generate_from_frequencies(frequencies)
+#cloud.to_file("myfile.jpg")
